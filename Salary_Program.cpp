b0092239689722419ec2bcf6a3492a580e9b1bbd
Mitchell Wolkow <44592262+Necromalleus@users.noreply.github.com>
2018-11-04 21:41:41 -0800

    Update Salary_Program.cpp

--- Salary_Program.cpp
+++ Salary_Program.cpp
@@ -9,11 +9,6 @@ int main() {
 	double rate;
 	double gross;
 
-	cout << "Author: Micthell Wolkow ID:0639384," << endl;
-	cout << "Program Name: Wolkow_Project1_Salary," << endl;
-	cout << "Date: October 3rd 2018," << endl;
-	cout << "College Name: Long Beach City College," << endl;
-
 	cout << "--------------------Employee Salary Records--------------------" << endl;
 
 	cout << "Input Employee ID Number: ";
